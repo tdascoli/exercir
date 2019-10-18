db89343ac23c9145bc1e450614be33536c55375f
Thomas D'Ascoli <thomas@dasco.li>
2019-10-18 21:32:22 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,78 @@
 var gulp        = require('gulp');
+var inject      = require('gulp-inject');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
-
-// BrowserSync Reload
-function reload(done) {
-  browserSync.reload();
-  done();
-}
+var minify      = require('gulp-minifier');
+var htmllint    = require('gulp-html');
 
 // Static Server + watching scss/html files
 gulp.task('watch', function () {
     browserSync.init({
-        server: './src'
+        server: './app'
     });
 
     gulp.watch("src/scss/*.scss", gulp.series('sass'));
-    gulp.watch(["src/*.html","src/*.js","src/*.css"]).on('change', browserSync.reload);
+    gulp.watch("src/scripts/*.js", gulp.series('js'));
+    gulp.watch("src/**/*.html", gulp.series('html'));
+    gulp.watch(["app/**/*"]).on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("src/scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(minify({
+          minify: true,
+          minifyCSS: true,
+          getKeptComment: function (content, filePath) {
+              var m = content.match(/\/\*![\s\S]*?\*\//img);
+              return m && m.join('\n') + '\n' || '';
+          }
+        }))
+        .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', gulp.series('sass','watch'));
+gulp.task('inject', () => {
+  return gulp.src(['./app/*.html'])
+          .pipe(inject(gulp.src(['./app/**/*.js',
+                                 './app/**/*.css'],
+                                 {read: false}), {relative: true}))
+          .pipe(gulp.dest('./app'))
+          .pipe(browserSync.stream());
+});
+
+
+gulp.task('js', function() {
+  return gulp.src('src/**/*.js').pipe(minify({
+    minify: true,
+    minifyJS: {
+      sourceMap: true
+    },
+    getKeptComment: function (content, filePath) {
+        var m = content.match(/\/\*![\s\S]*?\*\//img);
+        return m && m.join('\n') + '\n' || '';
+    }
+  }))
+  .pipe(gulp.dest('app'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('html', function(){
+  return gulp.src('src/*.html')
+    .pipe(minify({
+      minify: true,
+      minifyHTML: {
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+      },
+      getKeptComment: function (content, filePath) {
+          var m = content.match(/\/\*![\s\S]*?\*\//img);
+          return m && m.join('\n') + '\n' || '';
+      }
+    }))
+    .pipe(gulp.dest('app/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.series('js','sass','html','inject','watch'));
